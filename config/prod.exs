457fe860586dd0d489040e9abc4581d2c60a58c2
Gabriella Chronis <gabriella@puppetlabs.com>
2016-05-25 09:50:22 -0700

    Ready for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :story, Story.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "https://cryptic-falls-56232.herokuapp.com", port: 443],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
