5ef06f5cf28e932f04dbd23a0b9433f765a0e5c6
Gabriella Chronis <gabriella@puppetlabs.com>
2016-05-25 10:54:02 -0700

    Continuous deploy with Travis

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -4,6 +4,6 @@ defmodule Story.SessionController do
   def index(conn, _params) do
     # Return some static JSON for now
     conn
-    |> json(%{status: "Ok"})
+    |> json(%{status: "CI with Travis"})
   end
 end
